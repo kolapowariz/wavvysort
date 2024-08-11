35b7bc56cc82add53a03b28add008e2faab8f355
Kolapo Wariz <kolapowariz@gmail.com>
2024-08-11 11:33:29 +0100

    added a like button component

--- src/components/skeleton.tsx
+++ src/components/skeleton.tsx
@@ -6,10 +6,10 @@ export function DashboardSkeleton() {
       <div>
         <Skeleton className="p-4 w-full h-58 mt-2 mb-4 border-b-2 md:w-[50%]" >
           <div className="flex flex-col space-y-2 px-2">
-            <Skeleton className=" h-4 w-[240px] bg-white" />
-            <Skeleton className=" h-4 w-[200px] bg-white" />
-            <Skeleton className=" h-24 w-full bg-white" />
-            <Skeleton className=" h-2 w-[200px] bg-white" />
+            <Skeleton className=" h-4 w-[240px] bg-white dark:bg-current" />
+            <Skeleton className=" h-4 w-[200px] bg-white dark:bg-current" />
+            <Skeleton className=" h-24 w-full bg-white dark:bg-current" />
+            <Skeleton className=" h-2 w-[200px] bg-white dark:bg-current" />
             <div className="flex justify-center gap-2">
               <Skeleton className=" h-8 w-10 bg-white" />
               <Skeleton className=" h-8 w-10 bg-white" />
@@ -67,7 +67,7 @@ export function DashboardSkeleton() {
           </div>
         </Skeleton>
       </div>
-    
+
     </>
   )
 }
@@ -75,8 +75,14 @@ export function DashboardSkeleton() {
 export function PostSkeleton() {
   return (
     <div className="mx-auto">
-      <Skeleton className="mx-auto p-4 w-full h-full mt-2 mb-4 border-b-2 md:w-[50%]" >
+      <Skeleton className="mx-auto p-4 w-full h-full mb-4 border-b-2 md:w-[50%]" >
         <div className="flex flex-col space-y-2 px-2">
+          <div className="flex justify-center items-center md:gap-2 gap-4">
+            <Skeleton className=" h-10 w-[80px] bg-white dark:bg-current" />
+            <Skeleton className=" h-10 w-[80px] bg-white" />
+            <Skeleton className=" h-10 w-[80px] bg-white" />
+            <Skeleton className=" h-10 w-[80px] bg-white" />
+          </div>
 
           <Skeleton className=" h-4 w-[240px] bg-white" />
           <Skeleton className="h-[60vh] w-full bg-white" />
--- src/components/speak.tsx
+++ src/components/speak.tsx
@@ -3,15 +3,30 @@ import { SpeakProps } from "@/types/types";
 
 export default function Speak( { tit, content}: SpeakProps) {
   return (
-    <main>      
-      <button onClick={() => speak( tit, content)} className="py-2 px-4 rounded-lg mx-auto block bg-red-500">Read</button>
+    <main className="flex mb-4">      
+      <button onClick={() => speak( tit, content)} className="py-2 px-4 rounded-lg mx-auto block bg-[#09233c] text-white">Read</button>
+      <button onClick={() => pause()} className="py-2 px-4 rounded-lg mx-auto block bg-[#09233c] text-white">Pause</button>
+      <button onClick={() => play()} className="py-2 px-4 rounded-lg mx-auto block bg-[#09233c] text-white">Resume</button>
+      <button onClick={() => stop()} className="py-2 px-4 rounded-lg mx-auto block bg-[#09233c] text-white">Cancel</button>
     </main>
   );
 }
 
 
-
 const speak = (  tit: string, text: string) => {
   const utterance = new SpeechSynthesisUtterance(`${tit}. ${text}`);
   speechSynthesis.speak(utterance);
+};
+
+
+const pause = () => {
+  speechSynthesis.pause();
+};
+
+const play = () => {
+  speechSynthesis.resume();
+};
+
+const stop = () => {
+  speechSynthesis.cancel();
 };
