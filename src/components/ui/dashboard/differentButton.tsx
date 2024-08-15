ef68982ce71de1dd3f3b3641bdc6da1be7561bd6
Kolapo Wariz <kolapowariz@gmail.com>
2024-08-15 15:48:21 +0100

    added not sure to delete button

--- src/components/ui/dashboard/differentButton.tsx
+++ src/components/ui/dashboard/differentButton.tsx
@@ -2,24 +2,49 @@ import { deletePost } from '@/lib/action';
 import { TrashIcon, PencilIcon, PlusIcon, } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 
- 
+
 export function DeletePost({ id }: { id: string }) {
 
-  const deletePostWithId = deletePost.bind(null, id) 
+  const deletePostWithId = deletePost.bind(null, id)
   return (
-    <form action={deletePostWithId}>
-      <button type="submit" className="rounded-md border p-2 hover:bg-gray-100">
-        <span className="sr-only">Delete</span>
-        <TrashIcon className="w-4" />
-      </button>
-    </form>
+    // <form action={deletePostWithId}>
+    //   <button type="submit" className="rounded-md border p-2 hover:bg-gray-100">
+    //     <span className="sr-only">Delete</span>
+    //     <TrashIcon className="w-4" />
+    //   </button>
+    //   <AlertDialogComponent />
+
+    // </form>
+    <AlertDialog>
+      <AlertDialogTrigger><TrashIcon className="w-8" /></AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your post
+            and remove it from our servers.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <form action={deletePostWithId}>
+            <Button type="submit" >Delete
+              <span className="sr-only">Delete</span>
+            </Button>
+
+          </form>
+
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+
   );
 }
 
 
- 
+
 // ...
- 
+
 export function UpdatePost({ id }: { id: string }) {
   return (
     <Link
@@ -30,3 +55,44 @@ export function UpdatePost({ id }: { id: string }) {
     </Link>
   );
 }
+
+
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { Button } from '../button';
+
+// export function AlertDialogComponent() {
+//   return (
+//     <AlertDialog>
+//       <AlertDialogTrigger><TrashIcon className="w-8" /></AlertDialogTrigger>
+//       <AlertDialogContent>
+//         <AlertDialogHeader>
+//           <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+//           <AlertDialogDescription>
+//             This action cannot be undone. This will permanently delete your account
+//             and remove your data from our servers.
+//           </AlertDialogDescription>
+//         </AlertDialogHeader>
+//         <AlertDialogFooter>
+//           <AlertDialogCancel>Cancel</AlertDialogCancel>
+//           <form action="">
+//             <button type="submit">
+//               <AlertDialogAction>Continue</AlertDialogAction>
+//             </button>
+//           </form>
+//         </AlertDialogFooter>
+//       </AlertDialogContent>
+//     </AlertDialog>
+
+//   )
+// } className="rounded-md border p-2 hover:bg-gray-100"
